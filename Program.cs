7809d0ee58c8b554688b2feb31868a1136bf8b3b
leonkarlsson1 <leka05001@utb.vaxjo.se>
2023-01-19 12:14:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = "[{}]";
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in input)
+        {
+            if (c=='[' || c=='{')
+            {
+                stack.Push(c);
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine("Unbalanced");
+                return;
+            }
+            bool b = false;
+            char charStack = stack.Pop();
+            if (c=='}' && charStack=='{')
+            {
+                b = true;
+                return;
+            }
+            if (c==']' && charStack=='[')
+            {
+                b = true;
+                return;
+            }
+            if (!b){
+                Console.WriteLine("Unbalanced");
+                return;
+            }
+            
+        }
+        if (stack.Count==0)
+        {
+            Console.WriteLine("Balanced");
+        }
+        else{
+            Console.WriteLine("Unbalanced");
+        }
+    }
+}
